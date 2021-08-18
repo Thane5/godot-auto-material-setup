f1a52fbe30754ed85bec430d9f9f632300673346
Thane5 <13683581+Thane5@users.noreply.github.com>
2021-08-18 13:34:48 +0200

    Added Template material support

--- addons/quick_material_setup/TemplateLoader.gd
+++ addons/quick_material_setup/TemplateLoader.gd
@@ -2,4 +2,16 @@ tool
 extends HBoxContainer
 
 export(NodePath) var fileDialogPath
+export(NodePath) var rootControl
 
+
+
+
+func _on_OpenButton_pressed():
+	var fileDialog = get_node(fileDialogPath)
+	fileDialog.popup_centered()
+
+
+func _on_FileDialog_TemplateMat_file_selected(path):
+	get_node(rootControl).templateMatPath = path
+	get_node("LoadPath").set_text(path)
