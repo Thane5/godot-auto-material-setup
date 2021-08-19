e74bc0b4a59afd6962a3e5cb3d0373d0f8b452ae
Thane5 <13683581+Thane5@users.noreply.github.com>
2021-08-19 10:46:24 +0200

    Ui rework

---
+++ addons/quick_material_setup/ExpandArea.gd
@@ -0,0 +1,33 @@
+tool
+extends Button
+
+export(NodePath) var ExpandAreaPath
+
+
+# Called when the node enters the scene tree for the first time.
+func _enter_tree():
+	icon = get_icon('GuiTreeArrowDown', 'EditorIcons')
+	set_button_icon(icon)
+	var ExpandArea = get_node(ExpandAreaPath)
+	ExpandArea.set_visible(true)
+	var textColor = get_color("font_color", "Editor")
+	self.add_color_override('font_color', textColor)
+	self.add_color_override('font_color_hover', textColor)
+	self.add_color_override('font_color_pressed', textColor)
+	
+	self.add_color_override('icon_color', textColor)
+	self.add_color_override('icon_color_hover', textColor)	
+	self.add_color_override('icon_color_pressed', textColor)
+
+	
+func _toggled(button_pressed):
+	if(button_pressed) == true:
+		icon = get_icon('GuiTreeArrowDown', 'EditorIcons')
+		set_button_icon(icon)
+		var ExpandArea = get_node(ExpandAreaPath)
+		ExpandArea.set_visible(true)
+	else:
+		icon = get_icon('GuiTreeArrowRight', 'EditorIcons')
+		set_button_icon(icon)
+		var ExpandArea = get_node(ExpandAreaPath)
+		ExpandArea.set_visible(false)
